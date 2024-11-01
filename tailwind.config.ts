a1f74d623444ca9715c76c0908d1720dac726d67
Duarte <duarteda@protonmail.com>
2024-10-31 23:16:07 -0400

    tailwind setup

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html, ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
