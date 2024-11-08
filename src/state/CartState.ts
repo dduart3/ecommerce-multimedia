2eb1c7069da63b6535c0a4afe789d0db432da56a
Duarte <duarteda@protonmail.com>
2024-11-07 21:08:40 -0400

    added cart state manager

---
+++ src/state/CartState.ts
@@ -0,0 +1,66 @@
+import { CartController } from '../controllers/CartController';
+import { Product } from '../models/Product';
+import { ICartItem } from '../interfaces/ICart';
+
+export class CartState {
+  private static instance: CartState;
+  private cartController: CartController;
+  private subscribers: ((count: number) => void)[] = [];
+
+  private constructor() {
+    this.cartController = new CartController();
+    this.updateCartCount = this.updateCartCount.bind(this);
+  }
+
+  public static getInstance(): CartState {
+    if (!CartState.instance) {
+      CartState.instance = new CartState();
+    }
+    return CartState.instance;
+  }
+
+  getItems(): Map<string, ICartItem> {
+    return this.cartController.getItems();
+  }
+
+  getItemCount(): number {
+    return this.cartController.getItemCount();
+  }
+
+  getTotal(): number {
+    return this.cartController.getTotal();
+  }
+
+  getFormattedTotal(): string {
+    return this.cartController.getFormattedTotal();
+  }
+
+  addToCart(product: Product, quantity: number = 1) {
+    this.cartController.addToCart(product, quantity);
+    this.updateCartCount();
+  }
+
+  
+  updateQuantity(productId: string, quantity: number): void {
+    this.cartController.updateQuantity(productId, quantity);
+    this.updateCartCount();
+  }
+
+  removeFromCart(productId: string) {
+    this.cartController.removeFromCart(productId);
+    this.updateCartCount();
+  }
+
+  subscribe(callback: (count: number) => void) {
+    this.subscribers.push(callback);
+    callback(this.getItemCount());
+    return () => {
+      this.subscribers = this.subscribers.filter(cb => cb !== callback);
+    };
+  }
+
+  private updateCartCount() {
+    const count = this.getItemCount();
+    this.subscribers.forEach(callback => callback(count));
+  }
+}
