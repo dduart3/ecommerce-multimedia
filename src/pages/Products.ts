7c0e1956089932df6dd8cc6df627fd7d56ce1b35
Duarte <duarteda@protonmail.com>
2024-11-07 21:14:51 -0400

    minimal styles changes to ProductsPage

--- src/pages/Products.ts
+++ src/pages/Products.ts
@@ -1,38 +1,35 @@
 import { Page } from './Page';
 import { ProductController } from '../controllers/ProductController';
-import { CartController } from '../controllers/CartController';
 import { ProductCard } from '../components/product/ProductCard';
-//import { ProductCard } from '../components/product/ProductCard';
 
 export class ProductsPage extends Page {
   private productController: ProductController;
-  private cartController: CartController;
 
   constructor(containerId: string) {
     super(containerId);
     this.productController = new ProductController();
-    this.cartController = new CartController();
   }
 
   async render(): Promise<void> {
     this.container.innerHTML = /*html*/ `
       <app-header></app-header>
-      <div class="container mx-auto px-4 ">
-        <h1 class="text-4xl font-bold text-center mb-12">Our Products</h1>
+      <div class=" mx-auto px-16 pt-16 ">
+        <h1 class=" text-5xl font-bold text-center mb-12 font-orbitron">Productos</h1>
         
         <!-- Products Grid -->
-        <div id="products-grid" class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-4">
+        <div id="products-grid" class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 mt-4">
           <!-- Product cards will be inserted here -->
         </div>
       </div>
     `;
 
     const products = await this.productController.getAllProducts();
+    console.log(products);
     const grid = document.getElementById('products-grid');
     
     if (grid) {
       products.forEach(product => {
-        console.log(product);
+       
         const card = new ProductCard(product);
         grid.appendChild(card);
       });
