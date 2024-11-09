49aae718b149ee46cafd6034d23cc8a29c71a32b
Duarte <duarteda@protonmail.com>
2024-11-09 19:46:16 -0400

    refactored validation related types

--- src/interfaces/Auth.ts
+++ src/interfaces/Auth.ts
@@ -1,6 +1,7 @@
 export interface IRegisterData {
     email: string;
     password: string;
+    confirmPassword: string;
     firstName: string;
     lastName: string;
 }
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -6,42 +6,52 @@ export enum AuthOperationResult {
   NAME_TOO_SHORT = "NAME_TOO_SHORT",
   NAME_TOO_LONG = "NAME_TOO_LONG",
   INVALID_PASSWORD_FORMAT = "INVALID_PASSWORD_FORMAT",
-  WRONG_CREDENTIALS = "WRONG_CREDENTIALS",
+  INVALID_CREDENTIALS = "INVALID_CREDENTIALS",
   EMAIL_IN_USE = "EMAIL_IN_USE",
   USER_NOT_FOUND = "USER_NOT_FOUND",
   INVALID_NAME_FORMAT = "INVALID_NAME_FORMAT",
+  UNKNOW_ERROR = "UNKNOWN_ERROR",
+  INVALID_PASSWORD_CONFIRMATION = "INVALID_PASSWORD_CONFIRMATION",
 }
 
-export const AuthErrorMessages = {
+export const AuthOperationResultMessage = {
   [AuthOperationResult.SUCCESS]: 'Operación exitosa',
   [AuthOperationResult.INVALID_EMAIL_FORMAT]: 'Por favor, ingresa un correo electrónico válido',
+  [AuthOperationResult.INVALID_PASSWORD_CONFIRMATION]: 'Las contraseñas no coinciden',
   [AuthOperationResult.INVALID_PASSWORD_FORMAT]: 'La contraseña debe tener al menos 6 caracteres, debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial',
   [AuthOperationResult.EMAIL_IN_USE]: 'Este correo electrónico ya se encuentra registrado',
   [AuthOperationResult.USER_NOT_FOUND]: 'Usuario no encontrado',
-  [AuthOperationResult.WRONG_CREDENTIALS]: 'Usuario o contraseña incorrectos',
+  [AuthOperationResult.INVALID_CREDENTIALS]: 'Usuario o contraseña incorrectos',
   [AuthOperationResult.NAME_TOO_SHORT]: 'Nombre y apellido deben tener al menos 2 caracteres',
   [AuthOperationResult.NAME_TOO_LONG]: 'Nombre y apellido no pueden tener más de 30 caracteres',
   [AuthOperationResult.INVALID_NAME_FORMAT]: 'Nombre y apellido solo pueden contener letras',
+  [AuthOperationResult.UNKNOW_ERROR]: 'Ha ocurrido un error desconocido, por favor espera unos minutos e inténtalo de nuevo',
 } 
 
 export function validateRegisterData(registerData: IRegisterData): AuthOperationResult {
-  const { email, password, firstName, lastName } = registerData;
+  const { email, password, confirmPassword, firstName, lastName } = registerData;
   
   const isEmailValid = validateEmail(email);
   const isPasswordValid = validatePassword(password);
+  const isPasswordConfirmed = validatePasswordConfirmation(password, confirmPassword);
   const validateNameResult = validateName(firstName, lastName) ;
   
-  if (!isEmailValid) {
-    return AuthOperationResult.INVALID_EMAIL_FORMAT;
+  if (validateNameResult !== AuthOperationResult.SUCCESS){
+    return validateNameResult;
   }
 
-  if (!isPasswordValid) {
+  if (!isEmailValid){
+    return AuthOperationResult.INVALID_EMAIL_FORMAT;
+  }
+  
+  if (!isPasswordValid){
     return AuthOperationResult.INVALID_PASSWORD_FORMAT;
   }
 
-  if (validateNameResult !== AuthOperationResult.SUCCESS) {
-    return validateNameResult;
+  if (!isPasswordConfirmed){
+    return AuthOperationResult.INVALID_PASSWORD_CONFIRMATION;
   }
+  
   return AuthOperationResult.SUCCESS;
 }
 
@@ -49,42 +59,47 @@ export function validateLoginData(loginData: ILoginData): AuthOperationResult {
   const isEmailValid = validateEmail(loginData.email);
   const isPasswordValid = validatePassword(loginData.password);
 
-  if (!isEmailValid) {
+  if (!isEmailValid){
     return AuthOperationResult.INVALID_EMAIL_FORMAT;
   }
-
-  if (!isPasswordValid) {
+  
+  if (!isPasswordValid){
     return AuthOperationResult.INVALID_PASSWORD_FORMAT;
   }
   
   return AuthOperationResult.SUCCESS;
 }
 
-export function validateEmail(email: string): boolean {
-    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    return isEmailValid;
+function validateEmail(email: string): boolean {
+  const emailValidationRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const isEmailValid = emailValidationRegex.test(email);
+  return isEmailValid;
 }
 
-export function validatePassword(password: string): boolean {
-  const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/.test(password);
+function validatePassword(password: string): boolean {
+  const passwordValidationRegex =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/
+  const isPasswordValid = passwordValidationRegex.test(password);
   return isPasswordValid;
 }
 
-export function validateName(firstName: string, lastName: string): AuthOperationResult {
-  const nameRegex = /^[A-Za-z]+$/;
-  
-  if (!firstName || firstName.length < 2 || firstName.length > 30) {
-    return AuthOperationResult.NAME_TOO_SHORT;
-  }
-
-  if (!lastName || lastName.length < 2 || lastName.length > 30) {
-    return AuthOperationResult.NAME_TOO_LONG;
-  }
-
-  if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
-    return AuthOperationResult.INVALID_NAME_FORMAT;
-  }
-
-  return AuthOperationResult.SUCCESS;
+function validatePasswordConfirmation(password: string, confirmPassword: string): boolean {
+  return password === confirmPassword;
 }
 
+function validateName(firstName: string, lastName: string): AuthOperationResult {
+  const nameValidationRegex = /^[A-Za-z]+$/;
+  
+  if (!firstName || firstName.length < 2 || firstName.length > 30){
+    return AuthOperationResult.NAME_TOO_SHORT;
+  }
+  
+  if (!lastName || lastName.length < 2 || lastName.length > 30){
+    return AuthOperationResult.NAME_TOO_LONG;
+  }
+  
+  if (!nameValidationRegex.test(firstName) || !nameValidationRegex.test(lastName)){
+    return AuthOperationResult.INVALID_NAME_FORMAT;
+  }
+  
+  return AuthOperationResult.SUCCESS;
+}
