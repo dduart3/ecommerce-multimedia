abc101afbad298048b5ecc3e639a3dd09facc9ea
Duarte <duarteda@protonmail.com>
2024-11-11 01:43:11 -0400

    created stripe interface for return data

---
+++ src/interfaces/StripeInterfaces.ts
@@ -0,0 +1,25 @@
+export enum PaymentIntentStatus {
+    CANCELED = 'canceled',
+    SUCCEEDED = 'succeeded',
+    REQUIRES_PAYMENT_METHOD = 'requires_payment_method'
+}
+export interface IPaymentIntent {
+    id: string;
+    currency: string;
+    status: PaymentIntentStatus;
+    created: number;
+    description: string;
+    metadata: {
+      order_id: string;
+    };
+}
+
+export interface ICheckoutSession {
+    id: string;
+    amount_total: number;
+    customer_details: {
+      email: string;
+    };
+    payment_status: string;
+    created: number;
+}
