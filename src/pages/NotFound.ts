40fb4c7ae73d2836487a4aacd9d57672b8f93dc0
Duarte <duarteda@protonmail.com>
2024-11-11 01:43:26 -0400

    translated not found page to spanish

--- src/pages/NotFound.ts
+++ src/pages/NotFound.ts
@@ -6,9 +6,9 @@ export class NotFoundPage extends Page {
       <app-header></app-header>
       <div class="h-screen flex flex-col items-center justify-center">
         <h1 class="text-[120px] font-black">404</h1>
-        <p class="text-2xl mb-8">Page not found</p>
+        <p class="text-2xl mb-8">Página no encontrada</p>
         <a href="/" data-link class="bg-black text-white px-6 py-2 hover:bg-gray-800 transition-colors">
-          Back to Home
+          Volver  al inicio
         </a>
       </div>
     `;
