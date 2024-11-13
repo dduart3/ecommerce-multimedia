f5680aa446e9a1eb935eacccf8e98fe68682167f
Duarte <duarteda@protonmail.com>
2024-11-12 23:40:10 -0400

    small changes in products

--- src/components/product/ProductCard.ts
+++ src/components/product/ProductCard.ts
@@ -14,21 +14,21 @@ export class ProductCard extends Component {
 
   protected render(): void {
     this.setTemplate(/*html*/`
-      <div class="text-black bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300">
+      <div class="text-black bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300  max-w-72">
         <div class="aspect-w-1 aspect-h-1 w-full">
           <img src="/src/assets/images/products/${this.product.imageUrl}" 
                alt="${this.product.name}" 
                class="w-full h-[200px] object-contain bg-gray-50">
         </div>
         <div class="p-3 font-orbitron">
-          <h3 class="text-md font-bold mb-1 truncate">${this.product.name}</h3>
+          <h3 class="text-md font-bold mb-3 truncate">${this.product.name}</h3>
           <div class="flex justify-between items-center">
             <div class="flex flex-col justify-between gap-2">
-            <span class="text-sm text-black">Disponibles: ${this.product.stock}</span>
+            <span class="text-sm font-thin text-black">Disponibles: ${this.product.stock}</span>
             <span class="text-lg font-bold">${this.product.getFormattedPrice()}</span>
             </div>
             
-            <button class="add-to-cart bg-black text-white px-3 py-1 rounded text-sm hover:bg-gray-800 transition-colors ${this.product.stock === 0 ? 'opacity-50 cursor-not-allowed' : ''}" 
+            <button class="add-to-cart bg-black text-white px-2 py-2 rounded text-sm hover:bg-gray-800 transition-colors ${this.product.stock === 0 ? 'opacity-50 cursor-not-allowed' : ''}" 
                     ${this.product.stock === 0 ? 'disabled' : ''}>
               ${this.product.stock === 0 ? 'No disponible' : 'Meter al carrito'}
             </button>
--- src/pages/Products.ts
+++ src/pages/Products.ts
@@ -12,22 +12,19 @@ export class ProductsPage extends Page {
 
   async render(): Promise<void> {
     this.container.innerHTML = /*html*/ `
-      <app-header class="bg-dark"></app-header>
-
-      
-      <div class=" mx-auto  mt-20 bg-slate-50 w-full h-screen">
-      <h1 class="text-5xl font-bold text-center h-64 font-orbitron relative bg-[url('./src/assets/images/Techwear.jpg')] bg-right bg-contain bg-no-repeat grid place-items-center">
-      <div class="absolute inset-0 bg-black opacity-20"></div>
-      <span class="relative z-10 text-[90px]">Productos</span>
-  	  </h1>
+       <app-header class="bg-dark"></app-header>
+      <div class="mx-auto mt-20 bg-slate-50 w-full min-h-screen">
+        <h1 class="text-5xl font-bold text-center h-64 font-orbitron relative bg-[url('./src/assets/images/Techwear.jpg')] bg-right bg-contain bg-no-repeat grid place-items-center">
+          <div class="absolute inset-0 bg-black opacity-20"></div>
+          <span class="relative z-10 text-[90px]">Productos</span>
+        </h1>
         
-        <div class="flex mt-[-32px]"> 
-          <!-- Products Grid -->
-          <div id="products-grid" class="grid pt-10 px-28 grid-cols-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols- gap-8 mt-4">
+        <div class="container pl-12 "> 
+          <div id="products-grid" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 py-8">
             <!-- Product cards will be inserted here -->
           </div>
         </div>
-        </div>
+      </div>
         `;
 
     const products = await this.productController.getAllProducts();
